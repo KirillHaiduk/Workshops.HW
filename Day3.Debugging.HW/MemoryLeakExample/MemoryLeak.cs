2614d8f4745308ed7e2928ef76060aaea1480b69
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-07-02 17:16:34 +0300

    Added code example with Memory Leak

---
+++ Day3.Debugging.HW/MemoryLeakExample/MemoryLeak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MemoryLeakExample
+{
+    public class MemoryLeak
+    {
+        private SomeClass someClass;
+
+        public MemoryLeak(SomeClass someClass)
+        {
+            this.someClass = someClass;
+            someClass.SomeEvent += SomeClass_SomeEvent;
+        }
+
+        private void SomeClass_SomeEvent(object sender, EventArgs e)
+        {
+        }
+    }
+
+    public class SomeClass
+    {
+        public event EventHandler SomeEvent;
+    }
+}
---
+++ Day3.Debugging.HW/MemoryLeakExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MemoryLeakExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var list = new List<MemoryLeak>();
+            var someClass = new SomeClass();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(new MemoryLeak(someClass));
+            }
+        }
+    }
+}
