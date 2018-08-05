ec7cb22d5fe1a01eb8c1215c3701a783f2ad96d6
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-08-05 21:52:17 +0300

    Add files via upload

---
+++ ReflectionAndCachingHW/CachingTask/CachingSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace CachingTask
+{
+    public class CachingSystem
+    {
+    }
+}
